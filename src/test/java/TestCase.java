d51de37832a6e050df4d32c902f7c3d5d857e9da
oleg kundakov <oleg.kundakov.jazzteam@gmail.com>
2015-11-20 11:45:31 +0300

    добавил соединение но не работает

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -2,6 +2,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by JazzTeamUser on 20.11.2015.
  */
@@ -15,9 +19,17 @@ public class TestCase {
         }
     }
 
+    private Connection connection;
+
     @Before
     public void init() {
-
+        try {
+            connection =
+                    DriverManager.getConnection(
+                            "jdbc:oracle:thin:@localhost:1521:xe", "system", "admin");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
